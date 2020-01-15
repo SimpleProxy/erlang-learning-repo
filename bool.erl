f1305668f295f418fb35b2fbcf61d34040b0f597
Kelves <kelves_16@hotmail.com>
2020-01-15 17:35:04 -0300

    Credited source. Also added more boolean functions.

--- bool.erl
+++ bool.erl
@@ -1,15 +1,32 @@
 -module(bool).
 -export([b_not/1, b_or/2, b_and/2, b_nand/2]).
 
+%% Pratice code based on learnyousomeerlang.com examples
+%% Extended by the examples in evanmiller.org/joy-of-erlang
+
+%% NOT
 b_not(true) -> false;
 b_not(false) -> true.
 
-b_or(true, _) -> true;
-b_or(_, true) -> true;
+%% OR
 b_or(false, false) -> false.
+b_or(_, _) -> true;
 
+%% AND
 b_and(true, true) -> true;
-b_and(false, _) -> false;
-b_and(_, false) -> false.
+b_and(_, _) -> false;
 
+%% NOR
+b_nor(false, false) -> true;
+b_nor(_, _) -> false.
+
+%% NAND : the first composition.
 b_nand(Exp1, Exp2) -> b_not(b_and(Exp1, Exp2)).
+%% May also be seen as
+%% b_and(true, true) -> false;
+%% b_nand(_, _) -> true.
+
+%% XOR
+b_xor(A,A) -> false;
+b_xor(_,_) -> true.
+
