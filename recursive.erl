228996fee2cd0e929224adcbc3e21bd8f892d9a3
Kelves <kelves_16@hotmail.com>
2020-01-17 16:53:24 -0300

    Moved tail recursive funstions to tailrec.erl.

--- recursive.erl
+++ recursive.erl
@@ -1,31 +1,23 @@
 -module(recursive).
--export([factorial/1, duplicate/2, len/1, reverse/1,
+-export([factorial/1, len/1, reverse/1,
          sum/1, multiply/2, zip/2]).
 
-%% Sample code from learnyousomeerlang.com
-%% Just praticing with recursive and tail recursive functions
+%% Code praticing inspired in code from learnyousomeerlang.com
+%% Just praticing with recursive
+%% Tail recursive funtions were moved to another module
 
-len(List) -> len(List, 0).
+len([]) -> 0;
+len([_|T]) -> 1 + len(T).
 
-len([], Counter) -> Counter;
-len([_|Tail], Counter) -> len(Tail, Counter + 1).
+sum([]) -> 0;
+sum([H|T]) -> H + sum(T).
 
-duplicate(Number, Counter) -> duplicate(Number, Counter, []).
-
-duplicate(_, 0, List) -> List;
-duplicate(Number, Counter, List) -> duplicate(Number, Counter - 1,
-                                              [Number|List]).
-reverse(List) -> reverse(List, []).
-
-reverse([],RList) -> RList;
-reverse([Head|Tail],RList) -> reverse(Tail, [Head|RList]).
-
-sum(List) -> sum(List, 0).
-
-sum([], Accumulator) -> Accumulator;
-sum([Head|Tail], Accumulator) -> sum(Tail, Accumulator + Head).
+average([]) -> 0;
+average(List) -> sum(List)/len(List).
 
 multiply(List,Number) -> [Element * Number || Element <-  List].
+
+factorial(Number) -> Number * factorial(Number - 1).
 
 zip(List1, List2) -> reverse(zip(List1,List2, [])).
 
@@ -34,7 +26,6 @@ zip(_,[], Accumulator) -> Accumulator;
 zip([H1|T1], [H2|T2], Accumulator) -> 
     zip(T1, T2, [{H1, H2}|Accumulator]).
 
-factorial(Number) -> factorial(Number, 1).
 
 factorial(0, Accumulator) -> Accumulator;
 factorial(Number, Accumulator) when Number > 0 ->
