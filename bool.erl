94c76a865abd4f2099381ad8a79893aa6689d8e0
Kelves <kelves_16@hotmail.com>
2020-01-24 15:57:35 -0300

    Praticed good documentation principles. Fixed: export functions.

--- bool.erl
+++ bool.erl
@@ -1,20 +1,38 @@
--module(bool).
--export([b_not/1, b_or/2, b_and/2, b_nand/2]).
+%%%------------------------------------------------------------------
+%%% Pratice code based on learnyousomeerlang.com examples
+%%% Extended by the examples in evanmiller.org/joy-of-erlang
+%%%------------------------------------------------------------------
+%%% Description module bool
+%%%------------------------------------------------------------------
+%%% Basic boolean operations with two arguments.
+%%%------------------------------------------------------------------
+%%% b_not(signal)
+%%%     return the inverse of signal
+%%%------------------------------------------------------------------
+%%% b_or(signal1, signal2)
+%%%     boolean or
+%%%------------------------------------------------------------------
+%%% b_and(signal1, signal2)
+%%%     boolean and
+%%%------------------------------------------------------------------
+%%% b_nand(signal1, signal2)
+%%%     boolean nand
+%%%------------------------------------------------------------------
 
-%% Pratice code based on learnyousomeerlang.com examples
-%% Extended by the examples in evanmiller.org/joy-of-erlang
+-module(bool).
+-export([b_not/1, b_or/2, b_nor/2, b_and/2, b_nand/2, b_xor/2]).
 
 %% NOT
 b_not(true) -> false;
 b_not(false) -> true.
 
 %% OR
-b_or(false, false) -> false.
-b_or(_, _) -> true;
+b_or(false, false) -> false;
+b_or(_, _) -> true.
 
 %% AND
 b_and(true, true) -> true;
-b_and(_, _) -> false;
+b_and(_, _) -> false.
 
 %% NOR
 b_nor(false, false) -> true;
