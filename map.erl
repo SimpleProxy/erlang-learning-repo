65c6f7bd9e06d0fcd817cb2a30eae1e71e545015
Kelves <kelves_16@hotmail.com>
2020-01-27 16:53:19 -0300

    Filter and fold functions added.

--- map.erl
+++ map.erl
@@ -12,7 +12,26 @@
 %%%------------------------------------------------------------------
 
 -module(map).
--export([map/2]).
+-export([map/2, filter/2, fold/3]).
 
 map(_Fun, []) -> [];
 map(Fun, [Hd|Tl]) -> [Fun(Hd)|map(Fun, Tl)].
+
+%% Interface function that calls the internal filter function.
+filter(Fun, List) -> filter(Fun, List, []).
+
+%% Pass a list through a selector function, in case the element pass the
+%% filter it is added to the accumulator
+filter(_Fun, [], Accumulator) -> Accumulator;
+filter(Fun, [Hd|Tl], Acc) -> 
+    case Fun(Hd) of
+        true -> filter(Fun, Tl, [Hd|Acc]);
+        false -> filter(Fun, Tl, Acc)
+    end.
+
+%% Interface function that calls the internal fold function.
+% fold(Func, List) -> fold(Func, List, []).
+
+%% Internal fold.
+fold(_Func, [], Acc) -> Acc;
+fold(Func, [Hd|Tl], Acc) -> fold(Func, Tl, Func(Hd, Acc)).
