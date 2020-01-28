f05cc3870a2589884789afbca4a0ab94ff82f4dc
Kelves <kelves_16@hotmail.com>
2020-01-28 01:08:39 -0300

    Add nth/2 function, fixed malfunctions, deleted the used ones.

--- recursive.erl
+++ recursive.erl
@@ -27,14 +27,16 @@
 %%%------------------------------------------------------------------
 
 -module(recursive).
--export([factorial/1, len/1, reverse/1,
-         sum/1, multiply/2, zip/2]).
+-export([len/1, sum/1, reverse/1, average/1,
+         multiply/2, zip/2, factorial/1, nth/2]).
 
 len([]) -> 0;
 len([_|T]) -> 1 + len(T).
 
 sum([]) -> 0;
 sum([H|T]) -> H + sum(T).
+
+reverse([Hd|Tl]) -> [reverse(Tl)|Hd].
 
 average([]) -> 0;
 average(List) -> sum(List)/len(List).
@@ -50,8 +52,6 @@ zip(_,[], Accumulator) -> Accumulator;
 zip([H1|T1], [H2|T2], Accumulator) -> 
     zip(T1, T2, [{H1, H2}|Accumulator]).
 
-
-factorial(0, Accumulator) -> Accumulator;
-factorial(Number, Accumulator) when Number > 0 ->
-    factorial(Number - 1, Number * Accumulator).
+nth(0, [Hd|_Tl]) -> Hd;
+nth(Nth, [_Hd|Tl]) -> nth(Nth - 1, Tl).
 
